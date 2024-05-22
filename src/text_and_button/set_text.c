ea5a6b612de2e09e5cfd9ba98f5616540bfa2378
alexis clemot <alexis.clemot@epitech.eu>
2024-05-22 13:11:01 +0200

    ✨ text change in resolution feature

--- src/text_and_button/set_text.c
+++ src/text_and_button/set_text.c
@@ -51,9 +51,12 @@ void text_setting(app_t *app)
 
 void text_video(app_t *app)
 {
-    set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 1.5}, "Max Framerate: Vsync", 7);
-    set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 1.75}, "Fullscreen: OFF", 8);
+    set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 1.5}, "Max Fr"
+        "amerate: Vsync", 7);
+    set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 1.75}, "Fullsc"
+        "reen: OFF", 8);
     set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 2}, "Done", 9);
     set_text(app, (sfVector2f){1920 / 2, 1080 / 3}, "Video Settings", 24);
-    set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 1.25}, "Resolution: 1920/1080", 25);
+    set_text(app, (sfVector2f){1920 / 2, 1080 / 3 * 1.25}, "Resolu"
+        "tion: 1920/1080", 25);
 }
